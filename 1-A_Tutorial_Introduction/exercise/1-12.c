0407748a519a201fd80a2653a6d866c0cc27aec0
Joyounger <942510346@qq.com>
2017-04-08 00:34:14 +0800

    fix exercise 1-12
    Signed-off-by: Joyounger <942510346@qq.com>

--- 1-A_Tutorial_Introduction/exercise/1-12.c
+++ 1-A_Tutorial_Introduction/exercise/1-12.c
@@ -6,11 +6,27 @@
 int main(int argc, char const *argv[])
 {
 	int c;
+	int has_newline = 0;
 	while((c = getchar()) != EOF) {
-		if (c != ' ' && c!= '\t') {
+		if (c == ' ' || c == '\t' || c == '\n') {
+			if (has_newline == 0) {
+				putchar('\n');
+				has_newline = 1;
+			}
+		} else {
 			putchar(c);
+			has_newline = 0;
 		}
 	}
 
 	return 0;
 }
+
+
+/*
+kolya@asus ~/src/tcpl/1-A_Tutorial_Introduction/exercise $ ./1-12
+abc 	wwe 			rrr
+abc
+wwe
+rrr
+*/
