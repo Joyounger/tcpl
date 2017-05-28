2164ad7292d67e1583b2bc4f652277bd0ace4369
Joyounger <aquanox@163.com>
2017-05-28 15:42:06 +0800

    add question for 1-10
    Signed-off-by: Joyounger <aquanox@163.com>

--- 1-A_Tutorial_Introduction/exercise/1-10.c
+++ 1-A_Tutorial_Introduction/exercise/1-10.c
@@ -1,6 +1,13 @@
 // date:17.4.6
 // author: linyang <942510346@qq.com>
 
+
+/*
+Exercise 1-10. Write a program to copy its input to its output, replacing each tab by \t, each backspace by
+\b, and each backslash by \\. This makes tabs and backspaces visible in an unambiguous way.
+*/
+
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
