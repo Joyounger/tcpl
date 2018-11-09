71c96ada76b8e0ce9f99797aae3a3e3b141068d0
Joyounger <942510346@qq.com>
2018-11-09 16:18:49 +0800

    update 4-5
    Signed-off-by: Joyounger <942510346@qq.com>

--- 4-Functions_and_Program_Structure/exercise/4-5.c
+++ 4-Functions_and_Program_Structure/exercise/4-5.c
@@ -8,174 +8,168 @@ Exercise 4-5. Add access to library functions like sin, exp, and pow. See <math.
 
 
 #include <stdio.h>
-#include <stdlib.h> 	/* for atof() */
+#include <string.h>
+#include <string.h>
 #include <ctype.h>
+#include <math.h> /* for atof() */
 
-#define MAXOP 100 		/* max size of operand or operator */
-#define NUMBER '0' 		/* signal that a number was found */
-#define IDENTIFIER   1
-#define MAXVAL 100 		/* maximum depth of val stack */
+#define MAXOP 100       /* max size of operand or operator */
+#define NUMBER '0'      /* signal that a number was found */
+#define NAME   'n'      /* signal that a name was found */
 #define BUFSIZE 100
+#define MAXVAL 100
 
-char buf[BUFSIZE]; 		/* buffer for ungetch */
-double val[MAXVAL]; 	/* value stack */
-int bufp = 0; 			/* next free position in buf */
-int sp = 0; 			/* next free stack position */
+char buf[BUFSIZE];      /* buffer for ungetch */
+double val[MAXVAL];     /* value stack */
+int bufp = 0;           /* next free position in buf */
+int sp = 0;             /* next free stack position */
 
 
 int getop(char []);
 void push(double);
 double pop(void);
+void mathfnc(char []);
 int getch(void);
 void ungetch(int);
-
-
-double print_stack_top(void)
-{
-	if (sp > 0) {
-		printf("%f\n", val[sp]);
-		return val[sp];
-	} else {
-		printf("error: stack empty\n");
-		exit(-1);
-	}
-}
-
-void copy_stack_top(void)
-{
-	if (sp < MAXVAL) {
-		push(print_stack_top());
-	} else {
-		printf("error: stack full\n");
-	}
-}
-
-void swap_stack_top2(void)
-{
-	double top1 = pop();
-	double top2 = pop();
-
-	push(top1);
-	push(top2);
-}
-
-void clear_stack(void)
-{
-	int i;
-	for (i = 0; i < sp; i++) {
-		val[i] = 0;
-	}
-}
 
 /* reverse Polish calculator */
 int main(int argc, char const *argv[])
 {
-	int type;
-	double op2;
-	char s[MAXOP];
-	while ((type = getop(s)) != EOF) {
-		switch (type) {
-			case NUMBER:
-				push(atof(s));
-				break;
-			case '+':
-				push(pop() + pop());
-				break;
-			case '*':
-				push(pop() * pop());
-				break;
-			case '-':
-				op2 = pop();
-				push(pop() - op2);
-				break;
-			case '/':
-				op2 = pop();
-				if (op2 != 0.0)
-					push(pop() / op2);
-				else
-					printf("error: zero divisor\n");
-				break;
-			case '\n':
-				printf("\t%.8g\n", pop());
-				break;
-			default:
-				printf("error: unknown command %s\n", s);
-				break;
-		}
-	}
+    int type;
+    double op2;
+    char s[MAXOP];
 
-	return 0;
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case NAME:
+                mathfnc(s);
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+/* mathfnc: check string s for supported math functions */
+void mathfnc(char s[])
+{
+    double op2;
+
+    if (strcmp(s, "sin") == 0) {
+        push(sin(pop()));
+    } else if (strcmp(s, "cos") == 0) {
+        push(cos(pop()));
+    } else if (strcmp(s, "exp") == 0) {
+        push(exp(pop()));
+    } else if (strcmp(s, "pow") == 0) {
+        op2 = pop();
+        push(pow(pop(), op2));
+    } else {
+        printf("error: %s not supported\n", s);
+    }
 }
 
 /* push: push f onto value stack */
 void push(double f)
 {
-	if (sp < MAXVAL)
-		val[sp++] = f;
-	else
-		printf("error: stack full, can't push %g\n", f);
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
 }
 
 /* pop: pop and return top value from stack */
 double pop(void)
 {
-	if (sp > 0)
-		return val[--sp];
-	else {
-		printf("error: stack empty\n");
-	return 0.0;
-	}
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+    return 0.0;
+    }
 }
 
-/* getop: get next character or numeric operand */
+/* getop: get next character, numeric operand, or math fnc */
 int getop(char s[])
 {
-	int i, c;
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
-		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.')
-		return c;	/* not a number */
-	
-	i = 0;
+    int c,i;
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    i = 0;
 
-	if (isalpha(c)) {
-		while (isalpha(s[i++] = c = getch())) {
-			c = getch();
-		}
-		s[i - 1] = '\0';
-		if (c != EOF) {
-			ungetch(c);
-		}
+    if (islower(c)) {   /* command or NAME */
+        while (islower(s[++i] = c = getch()))
+            ;
+        s[i] = '\0';
+        if (c != EOF) {
+            ungetch(c);     /* went one char too far */
+        }
+        if (strlen(s) > 1) {
+            return NAME;    /* >1 char: it is NAME */
+        } else {
+            return c;       /* it may be a command */
+        }
+    }
 
-		return IDENTIFIER;
-	}
+    if (!isdigit(c) && c != '.') {
+        return c;       /* not a number */
+    }
+    if (isdigit(c))     /* collect integer part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')       /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
 
-	if (isdigit(c)) /* collect integer part */
-		while (isdigit(s[++i] = c = getch()))
-			;
-	if (c == '.') /* collect fraction part */
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
-
-	return NUMBER;
+    return NUMBER;
 }
-
-
 
 
 int getch(void) /* get a (possibly pushed-back) character */
 {
-	return (bufp > 0) ? buf[--bufp] : getchar();
+    return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
 void ungetch(int c) /* push character back on input */
 {
-	if (bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = c;
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
 }
+
+/*
+$ gcc -ansi -pedantic 4-5.c -o 4-5
+
+
+*/
